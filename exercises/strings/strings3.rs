14f53a0b5ea21aa6600d07748a366eee2bee16c1
Lumen Yang <lumen.yang@lumeny.io>
2023-11-04 21:36:59 +0100

    finished string

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,35 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    
+    let mut start = 0;
+    let mut end = input.len();
+    
+    for (i, char) in input.chars().enumerate() {
+        if char != ' ' {start = i;break;}
+    }
+    
+    for (i, char) in input.chars().rev().enumerate() {
+        if char != ' ' {end = input.len() - i;break;}
+    }
+
+    return input[start..end].to_string();
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut input = input.clone().to_string();
+
+    input.push_str(" world!");
+
+    input.to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons").to_string()
 }
 
 #[cfg(test)]
